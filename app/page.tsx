043ae5794c104e7de61aa6f4b93300a3f94a800c
SUBID DAS <itsmesubid@gmail.com>
2023-05-16 16:06:57 +0530

    Add `canvas`

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,119 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const Canvas = styled.canvas``;
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+    if (!canvas) return;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const ctx = canvas.getContext("2d");
+    window?.addEventListener("resize", () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      init();
+    });
+    window?.addEventListener("mousemove", ({ x, y }) => {
+      mouse.x = x;
+      mouse.y = y;
+    });
+    let mouse: {
+      x: number | undefined;
+      y: number | undefined;
+    } = {
+      x: undefined,
+      y: undefined,
+    };
+    let maxRadius = 100;
+    let range = 100;
+    let colors = ["#8ecae6", "#219ebc", "#023047", "#ffb703", "#fb8500"];
+    if (!ctx) return;
+    class Circle {
+      x: number;
+      y: number;
+      dx: number;
+      dy: number;
+      radius: number;
+      minRadius: number;
+      color: string;
+      constructor(
+        x: number,
+        y: number,
+        dx: number,
+        dy: number,
+        radius: number
+      ) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.minRadius = radius;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+      }
+      draw() {
+        if (!ctx) return;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+      }
+      update() {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+          this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+          this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        // interactivity
+        if (
+          mouse.x &&
+          mouse.y &&
+          Math.abs(mouse.x - this.x) < range &&
+          Math.abs(mouse.y - this.y) < range
+        ) {
+          if (this.radius < maxRadius) {
+            this.radius += 5;
+          }
+        } else if (this.radius > this.minRadius) {
+          this.radius -= 2.5;
+        }
+        this.draw();
+      }
+    }
+    let circleArray: Circle[] = [];
+    for (let i = 0; i < 200; i++) {
+      let radius = Math.random() * 20 + 5;
+      let x = Math.random() * (innerWidth - radius * 2) + radius;
+      let y = Math.random() * (innerHeight - radius * 2) + radius;
+      let dx = (Math.random() - 0.5) * 10;
+      let dy = (Math.random() - 0.5) * 10;
+      circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+    function animate() {
+      requestAnimationFrame(animate);
+      ctx?.clearRect(0, 0, innerWidth, innerHeight);
+      for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+      }
+    }
+    function init() {
+      circleArray = [];
+      for (let i = 0; i < 200; i++) {
+        let radius = Math.random() * 20 + 5;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dx = (Math.random() - 0.5) * 10;
+        let dy = (Math.random() - 0.5) * 10;
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+      }
+    }
+    animate();
+  }, []);
+  return <Canvas />;
 }
